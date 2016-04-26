d4e2929172aa90443f24a83bc4321a2c6a2aaf52
daavidjobe <daavid.jobe@gmail.com>
2016-04-26 17:01:16 +0200

    primary button

--- src/client/components/header/Header.js
+++ src/client/components/header/Header.js
@@ -2,8 +2,22 @@ import React from 'react';
 import './header.less';
 export default (props) => {
     return (
-        <div className="header">
+        <ul className="header">
+        <li>
           <img src="/src/client/assets/images/marks-logo.png" width="160" alt="marks logo"/>
-        </div>
+          </li>
+          <li>
+           <button className="button">
+            <p>log in</p>
+            <div className="underline"></div>
+            </button>
+          </li>
+          <li>
+           <button className="button">
+            <p>sign up</p>
+            <div className="underline"></div>
+            </button>
+          </li>
+        </ul>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './src/client/index.js',
   output: {
-    path: './',
+    path: './dist/',
     filename: 'dist/bundle.js'
   },
   devServer: {
