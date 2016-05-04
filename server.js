89801b9dc1eff35960b4cae920f47678c4f4bf4b
daavidjobe <daavid.jobe@gmail.com>
2016-05-04 13:38:02 +0200

    -

--- server.js
+++ server.js
@@ -1,45 +1,21 @@
-'use strict';
 
-var _path = require('path');
+var path = require('path');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000
 
-var _path2 = _interopRequireDefault(_path);
 
-var _express = require('express');
+app.use(express.static('dist'));
 
-var _express2 = _interopRequireDefault(_express);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var app = (0, _express2.default)();
-var PORT = process.env.PORT || 3000;
-
-// using webpack-dev-server and middleware in development environment
-if (process.env.NODE_ENV !== 'production') {
-  var webpackDevMiddleware = require('webpack-dev-middleware');
-  var webpackHotMiddleware = require('webpack-hot-middleware');
-  var webpack = require('webpack');
-  var config = require('./webpack.config');
-  var compiler = webpack(config);
-
-  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-  app.use(webpackHotMiddleware(compiler, {
-    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-  }));
-}
-
-app.use(_express2.default.static('dist'));
-
-app.get('/', function (request, response) {
-  console.log('hello from /');
-  response.sendFile(__dirname + '/dist/index.html');
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/dist/index.html')
 });
 
-app.get('*', function (request, response) {
-  console.log('hello from *');
-  response.sendFile(__dirname + '/dist/index.html');
+app.get('*', function(request, response) {
+  response.sendFile(__dirname + '/dist/index.html')
 });
 
-app.listen(PORT, function (error) {
+app.listen(PORT, function(error) {
   if (error) {
     console.error(error);
   } else {
