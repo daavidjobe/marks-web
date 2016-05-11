2a66b3f11269526fb648b9bd6851970b88aaf22c
daavidjobe <daavid.jobe@gmail.com>
2016-05-11 18:05:07 +0200

    -

--- src/client/sockets/MarkSocket.js
+++ src/client/sockets/MarkSocket.js
@@ -3,7 +3,7 @@ import MarkActions from '../actions/mark-actions';
 class MarkSocket {
 
   constructor() {
-    this.socket = new WebSocket("ws://" + location.hostname + "/api/socket/");
+    this.socket = new WebSocket("ws://" + location.hostname + ":4567/api/socket/");
     this.socket.onopen = function () {
       // socket is connected
       console.log('open');
@@ -12,12 +12,16 @@ class MarkSocket {
       console.log(e);
     };
     this.socket.onmessage = function (e) {
+      console.log('socket recieves message');
       let mark = JSON.parse(e.data);
+      console.log(mark);
       MarkActions.addPublicMark(mark);
     };
   }
   
   sendMessage(message) {
+    console.log('socket sends message');
+    console.log(message);
     this.socket.send(JSON.stringify(message));
   }
 
