6d33ae462a60dd255cc21a900750b33d0841397e
daavidjobe <daavid.jobe@gmail.com>
2016-05-18 10:36:08 +0200

    -

--- src/client/stores/user-store.js
+++ src/client/stores/user-store.js
@@ -13,51 +13,51 @@ import UserAPI from '../api/user-api';
 const CHANGE_EVENT = 'change';
 
 class UserStore extends EventEmitter {
-    
+
     constructor() {
         super();
         this.categories = [];
         this.marks = [];
-        this.user = {authenticated: this.isLoggedIn(), token: this.getToken()}
+        this.user = { authenticated: this.isLoggedIn(), token: this.getToken() }
     }
-    
+
     emitChange() {
         this.emit(CHANGE_EVENT)
     }
-    
+
     addChangeListener(callback) {
         this.on(CHANGE_EVENT, callback)
     }
-    
+
     removeChangeListener(callback) {
         this.removeListener(CHANGE_EVENT, callback)
     }
-    
+
     getUser() {
         return this.user;
     }
-    
+
     getEmail() {
         return localStorage.getItem('user');
     }
-    
+
     setUser(user) {
         this.user = user;
         localStorage.setItem('user', user.email);
     }
-    
+
     isLoggedIn() {
         return !!localStorage.token;
     }
-    
+
     getToken() {
         return localStorage.token;
     }
-    
+
     setToken(token) {
         localStorage.token = token
     }
-    
+
     logout() {
         delete localStorage.token;
         localStorage.removeItem('user');
@@ -74,50 +74,50 @@ class UserStore extends EventEmitter {
     getCategories() {
         return this.categories;
     }
-    
+
     addCategory(categoryName) {
-        let categories = [...this.categories, {name: categoryName}];
+        let categories = [...this.categories, { name: categoryName }];
         this.categories = categories;
     }
-    
+
     removeCategory(categoryName) {
         let updated = this.categories.filter(c => c.name !== categoryName);
         this.categories = updated;
     }
-    
+
     assignMarksToCategories() {
-       let assignedList = this.marks.map(mark => {
-         let cat = this.categories.filter(c => c.urls.indexOf(mark.url) !== -1)[0];
-         console.log(cat);
-         if(cat === undefined) {
-           mark.category = 'category';
-         } else {
-           mark.category = cat.name;
-         }
-         return mark;
-       })
-      this.marks = assignedList;
+        let assignedList = this.marks.map(mark => {
+            let cat = this.categories.filter(c => c.urls.indexOf(mark.url) !== -1)[0];
+            console.log(cat);
+            if (cat === undefined) {
+                mark.category = 'category';
+            } else {
+                mark.category = cat.name;
+            }
+            return mark;
+        })
+        this.marks = assignedList;
     }
-    
+
     setMarks(marks) {
         this.marks = marks;
     }
-    
+
     getMarks() {
         return this.marks;
     }
-    
-    
+
+
     addMark(mark) {
         mark.category = 'category';
         let marks = [mark, ...this.marks];
         this.marks = marks;
     }
-    
+
     removeMark(mark) {
         this.marks = this.marks.filter(m => m.url !== mark.url);;
     }
-    
+
     assignThumbnailToMark(meta, mark) {
         let marks = this.marks.filter(m => m.url !== mark.url);
         mark.thumbnail = meta.thumbnail;
@@ -127,46 +127,48 @@ class UserStore extends EventEmitter {
         mark.hasInteracted = false;
         MarkSocket.sendMessage(mark);
     }
-    
+
 }
-      
+
 let userStore = new UserStore();
 userStore.dispatchToken = register((action) => {
     switch (action.actionType) {
-            case UserConstants.LOGIN_COMPLETE:
-                userStore.setUser(action.user)
-                break;
-            case UserConstants.LOGOUT:
-                userStore.logout()
-                break;
-            case UserConstants.FETCH_CATEGORIES:
+        case UserConstants.LOGIN_COMPLETE:
+            userStore.setUser(action.user)
+            break;
+        case UserConstants.LOGOUT:
+            userStore.logout()
+            break;
+        case UserConstants.FETCH_CATEGORIES:
+            if (action.categories) {
                 userStore.setCategories(action.categories)
                 userStore.assignMarksToCategories()
-                break;
-            case UserConstants.ADD_CATEGORY:
-                userStore.addCategory(action.categoryName)
-                break;
-            case UserConstants.REMOVE_CATEGORY:
-                userStore.removeCategory(action.categoryName)
-                break;
-            case UserConstants.FETCH_MARKS:
-                userStore.setMarks(action.marks)
-                userStore.assignMarksToCategories()
-                break;
-            case MarkConstants.ADD_MARK:
-                userStore.addMark(action.data)
-                break;
-            case MarkConstants.REMOVE_MARK:
-                userStore.removeMark(action.data)
-                break;
-            case MarkConstants.FETCH_MARK_THUMBNAIL:
-                userStore.assignThumbnailToMark(action.meta, action.mark)
-                break;
-        }
-        userStore.emitChange();
+            }
+            break;
+        case UserConstants.ADD_CATEGORY:
+            userStore.addCategory(action.categoryName)
+            break;
+        case UserConstants.REMOVE_CATEGORY:
+            userStore.removeCategory(action.categoryName)
+            break;
+        case UserConstants.FETCH_MARKS:
+            userStore.setMarks(action.marks)
+            userStore.assignMarksToCategories()
+            break;
+        case MarkConstants.ADD_MARK:
+            userStore.addMark(action.data)
+            break;
+        case MarkConstants.REMOVE_MARK:
+            userStore.removeMark(action.data)
+            break;
+        case MarkConstants.FETCH_MARK_THUMBNAIL:
+            userStore.assignThumbnailToMark(action.meta, action.mark)
+            break;
+    }
+    userStore.emitChange();
 })
-   
-    
- 
+
+
+
 
 export default userStore;
