57cebd392a054c90ca75d3e66c2114570b6a3b3d
daavidjobe <daavid.jobe@gmail.com>
2016-05-18 10:57:16 +0200

    -

--- src/client/sockets/MarkSocket.js
+++ src/client/sockets/MarkSocket.js
@@ -4,24 +4,24 @@ import 'whatwg-fetch';
 class MarkSocket {
 
   constructor() {
-    fetch('/api/port').then(res => res.text())
-    .then(port => {
-      console.log(port);
-      this.socket = new WebSocket(`ws://${location.hostname}:${port}/api/socket/`);
-    this.socket.onopen = function () {
-      console.log('open');
-    };
-    this.socket.onerror = function (e) {
-      console.log(e);
-    };
-    this.socket.onmessage = function (e) {
-      console.log('socket recieves message');
-      let mark = JSON.parse(e.data);
-      MarkActions.addPublicMark(mark);
-    };
-    })
+    fetch('/api/webSocketUrl').then(res => res.text())
+      .then(url => {
+        console.log(url);
+        this.socket = new WebSocket(url);
+        this.socket.onopen = function () {
+          console.log('open');
+        };
+        this.socket.onerror = function (e) {
+          console.log(e);
+        };
+        this.socket.onmessage = function (e) {
+          console.log('socket recieves message');
+          let mark = JSON.parse(e.data);
+          MarkActions.addPublicMark(mark);
+        };
+      })
   }
-  
+
   sendMessage(message) {
     console.log('socket sends message');
     this.socket.send(JSON.stringify(message));
